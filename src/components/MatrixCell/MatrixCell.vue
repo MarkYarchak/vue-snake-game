b36594e045675049c49f8ad8b240914f494fd3c1
Mark Yarchak <markyarchak.work@gmail.com>
2020-12-12 18:39:24 +0200

    rounded dummy limbs

--- src/components/MatrixCell/MatrixCell.vue
+++ src/components/MatrixCell/MatrixCell.vue
@@ -46,17 +46,14 @@ export default {
       required: true,
     },
   },
+
   setup(props: Props) {
     const positions: MatrixPosition[] = reactive(game.dummy.positions);
     const position: MatrixPosition = reactive(props.cell.position);
 
     const computedCellColor = computed(() => {
-      try {
-        const isOdd = (position.row + position.column) % 2 === 0;
-        return isOdd ? GrassColor.Dark : GrassColor.Light;
-      } catch (e) {
-        return GrassColor.Red;
-      }
+      const isOdd = (position.row + position.column) % 2 === 0;
+      return isOdd ? GrassColor.Dark : GrassColor.Light;
     });
 
     const cellBgColor: GrassColor = computedCellColor.value;
@@ -64,50 +61,59 @@ export default {
     function getDummyPartStyles(): object {
       if (props.cell.content !== CellType.Dummy) return {};
 
-      const positionIndex: number = positions
-        .findIndex(p => p.row === position.row && p.column === position.column);
-
-      const closestFrontPosition: MatrixPosition = positions[positionIndex + 1];
-      const closestBackPosition: MatrixPosition = positions[positionIndex - 1];
-
+      const positionIndex: number = findDummyPartIndex(position);
+      const closestFrontPosition: MatrixPosition = positions[positionIndex - 1];
+      const closestBackPosition: MatrixPosition = positions[positionIndex + 1];
       const closestPositions = [closestFrontPosition, closestBackPosition];
-      if (isDummyLimb(closestPositions)) {
-        // TODO: if is front limb set brs to (for example) 0 0 50% 50%
-        // else if back limb set opposite brs
 
-        return {};
-      }
-
-      let dummyPartBorderRadius = '';
-
-      switch (true) {
-        case checkTurn(DummyTurn.TopLeft, closestPositions): dummyPartBorderRadius = '0 0 50% 0'; break;
-        case checkTurn(DummyTurn.TopRight, closestPositions): dummyPartBorderRadius = '0 0 0 50%'; break;
-        case checkTurn(DummyTurn.RightBottom, closestPositions): dummyPartBorderRadius = '50% 0 0 0'; break;
-        case checkTurn(DummyTurn.LeftBottom, closestPositions): dummyPartBorderRadius = '0 50% 0 0'; break;
-        default: dummyPartBorderRadius = ''; break;
-      }
+      const dummyPartBorderRadius: string = isDummyLimb(closestPositions)
+        ? calcLimbBorderRadius(closestFrontPosition || closestBackPosition)
+        : calcTurnBorderRadius(closestPositions);
 
       return {
         borderRadius: dummyPartBorderRadius,
       };
     }
 
+    function findDummyPartIndex({ row, column }: MatrixPosition): number {
+      return positions.findIndex(p => p.row === row && p.column === column);
+    }
+
     function isDummyLimb(closestPositions: MatrixPosition[]): boolean {
-      return !(closestPositions[0] && closestPositions[1]);
+      return !closestPositions[0] || !closestPositions[1];
+    }
+
+    function calcLimbBorderRadius(closestPosition: MatrixPosition): string {
+      switch (true) {
+        case closestPosition.row + 1 === position.row: return '0 0 50% 50%';
+        case closestPosition.row - 1 === position.row: return '50% 50% 0 0';
+        case closestPosition.column + 1 === position.column: return '0 50% 50% 0';
+        case closestPosition.column - 1 === position.column: return '50% 0 0 50%';
+        default: return '';
+      }
+    }
+
+    function calcTurnBorderRadius(closestPositions: MatrixPosition[]): string {
+      switch (true) {
+        case checkTurn(DummyTurn.TopLeft, closestPositions): return '0 0 50% 0';
+        case checkTurn(DummyTurn.TopRight, closestPositions): return '0 0 0 50%';
+        case checkTurn(DummyTurn.RightBottom, closestPositions): return '50% 0 0 0';
+        case checkTurn(DummyTurn.LeftBottom, closestPositions): return '0 50% 0 0';
+        default: return '';
+      }
     }
 
     function checkTurn(turn: DummyTurn, closestPositions: MatrixPosition[]): boolean {
       switch (turn) {
-        case DummyTurn.TopLeft: return checkTwoSidesTurn(closestPositions, isTurnTopLeft);
-        case DummyTurn.TopRight: return checkTwoSidesTurn(closestPositions, isTurnTopRight);
-        case DummyTurn.RightBottom: return checkTwoSidesTurn(closestPositions, isTurnRightBottom);
-        case DummyTurn.LeftBottom: return checkTwoSidesTurn(closestPositions, isTurnLeftBottom);
+        case DummyTurn.TopLeft: return checkWithReverseSidesTurn(closestPositions, isTurnTopLeft);
+        case DummyTurn.TopRight: return checkWithReverseSidesTurn(closestPositions, isTurnTopRight);
+        case DummyTurn.RightBottom: return checkWithReverseSidesTurn(closestPositions, isTurnRightBottom);
+        case DummyTurn.LeftBottom: return checkWithReverseSidesTurn(closestPositions, isTurnLeftBottom);
         default: throw new Error('Invalid turn type');
       }
     }
 
-    function checkTwoSidesTurn(closestPositions: MatrixPosition[], checkFunc: (closestPositions: MatrixPosition[]) => boolean): boolean {
+    function checkWithReverseSidesTurn(closestPositions: MatrixPosition[], checkFunc: (closestPositions: MatrixPosition[]) => boolean): boolean {
       return checkFunc(closestPositions) || checkFunc(closestPositions.reverse());
     }
 
@@ -137,6 +143,7 @@ export default {
       game,
     };
   },
+
 };
 </script>
 
