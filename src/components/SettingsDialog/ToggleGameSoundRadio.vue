4307853150b0b2018db7d9254abfc16123e43933
Mark Yarchak <markyarchak.work@gmail.com>
2025-01-06 13:27:13 +0200

    feat: sync settings use sound toggle

--- src/components/SettingsDialog/ToggleGameSoundRadio.vue
+++ src/components/SettingsDialog/ToggleGameSoundRadio.vue
@@ -4,11 +4,23 @@
 
     <div>
       <label>
-        <input type="radio" class="nes-radio" name="answer" checked />
+        <input
+          :checked="modelValue"
+          type="radio"
+          name="answer"
+          class="nes-radio"
+          @change="modelValue = true"
+        />
         <span>Yes</span>
       </label>
       <label class="ml-2">
-        <input type="radio" class="nes-radio" name="answer" />
+        <input
+          :checked="!modelValue"
+          type="radio"
+          name="answer"
+          class="nes-radio"
+          @change="modelValue = false"
+        />
         <span>No</span>
       </label>
     </div>
@@ -16,5 +28,10 @@
 </template>
 
 <script setup lang="ts">
+import { defineModel } from 'vue';
 
+const modelValue = defineModel({
+  type: Boolean,
+  default: false,
+});
 </script>
